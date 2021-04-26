bfc84058629211cb1c9ad13a968307bde515aff7
Eddie02582 <38853252+Eddie02582@users.noreply.github.com>
2021-04-26 14:35:31 +0800

    update

--- 讀寫Excel xls/example_xlwt.py
+++ 讀寫Excel xls/example_xlwt.py
@@ -1,52 +1,3 @@
-# 讀寫Excel xls 檔
-
-
-## 讀取Excel
-
-```
-	pip install xlrd
-```
-
-```python 
-
-import xlrd
-path = r"test.xls"
-
-#get workbook
-workbook = xlrd.open_workbook(path)
-
-#get sheet_names
-workbook.sheet_names()
-
-#get worksheet by name
-worksheet = workbook.sheet_by_name("test1")
-
-#get worksheet by index
-worksheet = workbook.sheet_by_name("test1"
-
-#get cell value row = 2 col = 3 (start from 0)
-worksheet.cell_value(1,2) 
-
-# get max row
-worksheet.nrows
-
-# get max column
-worksheet.ncols
-
-)
-
-
-```
-
-## 寫入Excel
-
-```
-	pip install xlwt
-```
-
-
-```python 
-
 from xlwt import Workbook, XFStyle, Borders, Pattern, Font,Formula,Alignment
 from datetime import date
 
@@ -135,13 +86,6 @@ worksheet.col(4).width = 256 *30
 #write value with style
 worksheet.write(0,4,date(2009,3,18),style)
 workbook.save('example.xls')
-
-
-```
-
-
-
-
 
 
 
